e63c06042b8ee859334e1b663795e74c4a001414
ChubbyI <chukwubuikem.idigo@gmail.com>
2024-02-06 10:54:09 -0500

    update to var terraform scripts

--- Terraform showcase/Variables Terraform/instance.tf
+++ Terraform showcase/Variables Terraform/instance.tf
@@ -1,4 +1,11 @@
-resource "aws_instance" "intro" {
+resource "aws_instance" "new-instance" {
     ami = var.AMIS[var.REGION]
     instance_type = "t2.micro"
+    availability_zone = var.ZONE1
+    key name = "enter kewy name here"
+    vpc_security_group_ids = ["enter security group id here"]
+    tags = {
+        Name = "new-instance"
+        # Enter whatever tag you need to identify this instance
+    }
 }
