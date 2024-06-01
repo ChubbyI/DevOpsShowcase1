4ca6a8d19b0eb44e42bb2d5cdcff7203d6ba7049
Chubby <chukwubuikem.idigo@gmail.com>
2024-05-31 21:50:33 -0400

    auto docker image build &push

--- Scripts/create-Docker-image.sh
+++ Scripts/create-Docker-image.sh
@@ -12,3 +12,6 @@ docker build -t "chubbyi/${project_name}-${current_branch}-${date_now}:${version
 
 # Push to Docker Hub
 docker push "chubbyi/${project_name}-${current_branch}-${date_now}:${version}"
+
+
+# Ensure your workstation has the proper permision for the docker account you are trying to push to and change the username from "chubbyi" to your user name in the build and push steps. 
